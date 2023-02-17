e3a54445a0f94ce8d7e8cc4d96f094c1d9ed82d5
Jacob Boyles <fixterjake@gmail.com>
2023-02-17 10:50:04 -0500

    Update email views

--- resources/views/emails/event/bulkEmail.blade.php
+++ resources/views/emails/event/bulkEmail.blade.php
@@ -8,5 +8,6 @@ Dear **{{ $full_name }}**,
 @lang('Regards'),
 
 **{{ config('mail.from.name', config('app.name')) }}**
-This mailbox is not being monitored. Please do not reply to this email. If you have any queries please direct them to the Cross The Land staff members via Discord.
+@break
+This mailbox is not being monitored. Please do not reply to this email. If you have any queries please direct them to the VATSIM Community Discord which you may join at https://community.vatsim.net
 @endcomponent
